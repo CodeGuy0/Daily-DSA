5e855fc0b75d56a92ea1bc43d96c30f361af93a0
Pranay Kumar Jha <125690497+CodeGuy0@users.noreply.github.com>
2024-04-12 02:35:29 +0530

    Update Arrays_Questions.java

--- Arrays_Questions.java
+++ Arrays_Questions.java
@@ -31,19 +31,19 @@ public class Arrays_Questions {
 		
 		// Q2:
 		
-		int T = sc.nextInt();
-		for (int t=0; t<T; t++) {
-			int n = sc.nextInt();
-			int [] arr = new int[n];
-			for (int i=0; i<n; i++) {
-				arr[i] = sc.nextInt();
-			}
+		// int T = sc.nextInt();
+		// for (int t=0; t<T; t++) {
+		// 	int n = sc.nextInt();
+		// 	int [] arr = new int[n];
+		// 	for (int i=0; i<n; i++) {
+		// 		arr[i] = sc.nextInt();
+		// 	}
 			
-			printPositiveCumulativeSum(arr);
+		// 	printPositiveCumulativeSum(arr);
 			
-		}
-		sc.close();
-		}
+		// }
+		// sc.close();
+		// }
 	
 //	Q1. The cumulative sum of an array at index i is defined as the sum of all elements of the array from index 0 to index i.
 	
@@ -60,18 +60,18 @@ public class Arrays_Questions {
 	
 	
 //	Q2. The cumulative sum of an array at index i is defined as the sum of all elements of the array from index 0 to index i.
-	private static void printPositiveCumulativeSum(int[] arr) {
-		int sum=0;
-		for(int i=0; i<arr.length; i++) {
-			sum+=arr[i];
-			if(sum>0) {
-				System.out.print(sum + " ");
-			} else {
-				sum=0;
-			}
+	// private static void printPositiveCumulativeSum(int[] arr) {
+	// 	int sum=0;
+	// 	for(int i=0; i<arr.length; i++) {
+	// 		sum+=arr[i];
+	// 		if(sum>0) {
+	// 			System.out.print(sum + " ");
+	// 		} else {
+	// 			sum=0;
+	// 		}
 		
-	}
-		System.out.println();
+	// }
+	// 	System.out.println();
 	
 	
 	
