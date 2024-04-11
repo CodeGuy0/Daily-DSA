b4d1576d3161f91f2a29cde61bdbcf8986db8c09
Pranay Kumar Jha <125690497+CodeGuy0@users.noreply.github.com>
2024-04-12 02:33:12 +0530

    Add files via upload

---
+++ Arrays_Questions.java
@@ -0,0 +1,80 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Arrays_Questions {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		//Q1 : 
+		
+//		int T = sc.nextInt();
+//		for (int t=0; t<T;t++) {
+//			int n = sc.nextInt();
+//			int[] arr = new int[n];
+//			for (int i=0; i<n; i++) {
+//				arr[i] = sc.nextInt();
+//			}
+//			
+//			int[] cumSum = getCumulativeSum(arr);
+//			for (int i = 0; i<n; i++) {
+//				System.out.print(cumSum[i] + " ");
+//				
+//			}
+//			System.out.println();
+//			
+//		}
+//		sc.close();
+		
+		
+		// Q2:
+		
+		int T = sc.nextInt();
+		for (int t=0; t<T; t++) {
+			int n = sc.nextInt();
+			int [] arr = new int[n];
+			for (int i=0; i<n; i++) {
+				arr[i] = sc.nextInt();
+			}
+			
+			printPositiveCumulativeSum(arr);
+			
+		}
+		sc.close();
+		}
+	
+//	Q1. The cumulative sum of an array at index i is defined as the sum of all elements of the array from index 0 to index i.
+	
+//	private static int[] getCumulativeSum(int[] arr) {
+//		int[] cumSum = new int[arr.length];
+//		cumSum[0] = arr[0];
+//		for (int i=1; i<arr.length; i++) {
+//			cumSum[i] = cumSum[i-1] + arr[i];
+//			
+//		}
+//		return cumSum;
+		
+	
+	
+	
+//	Q2. The cumulative sum of an array at index i is defined as the sum of all elements of the array from index 0 to index i.
+	private static void printPositiveCumulativeSum(int[] arr) {
+		int sum=0;
+		for(int i=0; i<arr.length; i++) {
+			sum+=arr[i];
+			if(sum>0) {
+				System.out.print(sum + " ");
+			} else {
+				sum=0;
+			}
+		
+	}
+		System.out.println();
+	
+	
+	
+	}
+
+}
