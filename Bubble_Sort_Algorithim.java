a4410f23d649b45dde51a3aaee4c96e781f0ec7d
Pranay Kumar Jha <125690497+CodeGuy0@users.noreply.github.com>
2024-07-21 05:28:01 +0530

    Update Bubble_Sort_Algorithim.java
    optimized way

--- Bubble_Sort_Algorithim.java
+++ Bubble_Sort_Algorithim.java
@@ -36,3 +36,60 @@ public class Bubble_Sort_Algorithim {
 	}
 
 }
+
+
+
+
+//  Optimized Way :: 
+
+
+package arrays;
+
+public class Bubble_Sort_Algorithim {
+	public static void main(String[] args) {
+		
+		int [] a = {7,6,5,4,3};
+		bubbleSort(a);
+		for (int i:a) {
+			System.out.print(i + " ");
+		}
+		
+	}
+	
+	static void bubbleSort(int[] a) {
+		int n = a.length;
+		
+		// n-1 iteration/passes 
+		for (int i=0; i<n-1;i++) {
+			boolean flag = false;
+			
+			for (int j=0; j<n-i-1;j++) {
+				
+				/*
+				 * last i elements are already at correct sorted positions,
+				 * so no need to check them
+				 * 
+				 */
+				
+				if (a[j]> a[j+1]) {
+					// swap - a[j], a[j+1]
+					int temp = a[j];
+					a[j] = a[j+1];
+					a[j+1] = temp;
+					flag = true; // some swap has happened.
+					
+				}
+			}
+			
+			if (flag == false  ) { // have any swaps happened ?
+				return;
+				
+			}
+		}
+		
+	}
+
+}
+
+
+
